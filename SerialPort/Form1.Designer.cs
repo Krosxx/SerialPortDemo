1209e72d67c7408c50a1d42dd8d61b7e43fb346c
Vove <1132412166@qq.com>
2017-11-06 14:50:00 +0800

    添加项目文件。

---
+++ SerialPort/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿using System.IO.Ports;
+
+namespace SerialPortDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series19 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series20 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series21 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.RichTextBox();
+            this.comboPort = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboRate = new System.Windows.Forms.ComboBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(393, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "打开";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(3, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 380);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "";
+            // 
+            // comboPort
+            // 
+            this.comboPort.FormattingEnabled = true;
+            this.comboPort.Items.AddRange(new object[] {
+            "COM3"});
+            this.comboPort.Location = new System.Drawing.Point(70, 25);
+            this.comboPort.Name = "comboPort";
+            this.comboPort.Size = new System.Drawing.Size(101, 23);
+            this.comboPort.TabIndex = 3;
+            this.comboPort.Text = "COM3";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "端口：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(204, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "频率：";
+            // 
+            // comboRate
+            // 
+            this.comboRate.FormattingEnabled = true;
+            this.comboRate.Items.AddRange(new object[] {
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.comboRate.Location = new System.Drawing.Point(262, 25);
+            this.comboRate.Name = "comboRate";
+            this.comboRate.Size = new System.Drawing.Size(101, 23);
+            this.comboRate.TabIndex = 6;
+            this.comboRate.Text = "9600";
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea7.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea7);
+            legend7.Name = "Legend1";
+            this.chart1.Legends.Add(legend7);
+            this.chart1.Location = new System.Drawing.Point(168, 86);
+            this.chart1.Name = "chart1";
+            series19.ChartArea = "ChartArea1";
+            series19.Legend = "Legend1";
+            series19.Name = "u1";
+            series20.ChartArea = "ChartArea1";
+            series20.Legend = "Legend1";
+            series20.Name = "u2";
+            series21.ChartArea = "ChartArea1";
+            series21.Legend = "Legend1";
+            series21.Name = "k";
+            this.chart1.Series.Add(series19);
+            this.chart1.Series.Add(series20);
+            this.chart1.Series.Add(series21);
+            this.chart1.Size = new System.Drawing.Size(559, 380);
+            this.chart1.TabIndex = 7;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 478);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.comboRate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboPort);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox textBox1;
+        private System.Windows.Forms.ComboBox comboPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboRate;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ SerialPort/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace SerialPortDemo
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort = new SerialPort();
+        private int received_count = 0;
+        private StringBuilder builder = new StringBuilder();
+        private String lastData = "";
+        public Form1()
+        {
+            InitializeComponent();
+            initSerialPort();
+            
+            initChart();
+        }
+        private void initSerialPort()
+        {
+            this.serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+            //this.serialPort.PortName = "COM3"; //端口号 
+            this.serialPort.Parity = 0; //奇偶校验 
+            //this.serialPort.BaudRate = 9600;//串口通信波特率 
+            this.serialPort.DataBits = 8; //数据位 
+            this.serialPort.StopBits = (StopBits)1;//停止位 
+            this.serialPort.ReadTimeout = 1000; //读超时 
+        }
+        private void initChart()
+        {
+            chart1.Series[0].Color = Color.Red;
+            chart1.Series[0].ChartType = SeriesChartType.Spline;
+            chart1.Series[1].ChartType = SeriesChartType.Line;
+            chart1.Series[2].ChartType = SeriesChartType.Spline;
+
+            chart1.Series[1].Color = Color.Green;
+            chart1.Series[2].Color = Color.Blue;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lastData = "";
+            if (serialPort.IsOpen)
+            {
+                //串口状态是open时，text是关闭 点击，则关闭串口
+                serialPort.Close();
+                button1.Text = "打开";
+                //textBox1.Text = "";
+            }
+            else
+            {
+                if (comboPort.Text == "")
+                {
+                    MessageBox.Show("选择串口！");
+                    return;
+                }
+                if (comboRate.Text == "")
+                {
+                    MessageBox.Show("设置波特率！");
+                    return;
+                }
+                serialPort.PortName = comboPort.Text;
+                serialPort.BaudRate = int.Parse(comboRate.Text);
+                try
+                {
+                    serialPort.Open();
+                    button1.Text = "关闭";
+                }
+                catch (Exception e1)
+                {
+                    //捕获到异常信息，创建一个新的comm对象，之前的不能用了。
+                    serialPort = new SerialPort();
+                    //显示异常信息给客户。
+                    MessageBox.Show(e1.Message);
+                }
+
+            }
+
+        }
+        //接收串口数据函数
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            int n = serialPort.BytesToRead;//先记录下来，避免某种原因，人为的原因，操作几次之间时间长，缓存不一致
+
+            byte[] buf = new byte[n];//声明一个临时数组存储当前来的串口数据
+            received_count += n;//增加接收计数
+            serialPort.Read(buf, 0, n); ;//读取缓冲数据
+
+            builder.Clear();//清除字符串构造器的内容
+            //因为要访问ui资源，所以需要使用invoke方式同步ui。
+            this.Invoke((EventHandler)(delegate
+            {
+                //直接按ASCII规则转换成字符串
+                String data = Encoding.ASCII.GetString(buf);
+                //
+                String[] d = data.Split('#');
+                //lastData = d[d.Length - 1];
+                if (data.Equals("#")|| d.Length == 1 || (!d[0].Equals("")&&d[1].Equals("")))
+                {
+                    if ((data[data.Length - 1]!='#'))
+                    {
+                        lastData += data;
+                        return;
+                    }
+                    else//遇到‘#’
+                    {
+                        lastData += data.Trim('#');
+                        String[] dataNum = lastData.Split(',');
+                        if (dataNum.Length != 3)
+                            return;
+                        for (int j = 0; j < 3; j++)
+                        {
+                            try
+                            {
+                            drawChart(j, double.Parse(dataNum[j]));
+
+                            }
+                            catch
+                            {
+                                lastData = "";
+                                return;
+                            }
+                        }
+
+                        builder.Append(lastData + "\n");
+                        textBox1.AppendText(builder.ToString());
+                        lastData = "";
+
+                    }
+                }
+                else//缓存数据筛去
+                {
+                    String[] t=data.Split('#');
+                    lastData = t[t.Length - 1];
+                    return;
+                }
+
+
+            }));
+
+
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+        }
+
+        //绘图
+        private void drawChart(int index, double y)
+        {
+            chart1.Series[index].Points.AddY(y);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            serialPort.Close();
+        }
+    }
+}
